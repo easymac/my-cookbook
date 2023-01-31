ace1221d5c0de4a853c595cfb3b27ff7ed5a0426
easymac <mac@ezm.ac>
2023-01-31 14:53:47 -0700

    Make font available to CSS globally

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,5 +1,6 @@
 import { Public_Sans } from '@next/font/google'
 
 export const publicSans = Public_Sans({
-  subsets: ['latin']
+  subsets: ['latin'],
+  variable: '--public-sans'
 })
--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={publicSans.className}>
+    <html lang="en" className={publicSans.variable}>
       <head />
       <body>
         <Container>
