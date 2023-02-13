1e70f73ff13a883896e555e538b7cda85f9e14a3
easymac <mac@ezm.ac>
2023-02-13 08:29:50 -0700

    Change search behavior on recipe pages

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,6 +1,6 @@
 'use client'
-import { useContext, useState } from 'react'
-import { useSearchParams, useRouter } from 'next/navigation'
+import { useContext, useState, useRef } from 'react'
+import { useSearchParams, useRouter, usePathname } from 'next/navigation'
 import { FiSearch } from 'react-icons/fi'
 import { MdFilterList } from 'react-icons/md'
 import { Button } from '@/components/ui/Button'
@@ -8,14 +8,30 @@ import { FilterMenu } from '@/components/FilterMenu'
 import styles from '@/components/SearchBar.module.css'
 
 export function SearchBar({ toggleFilterMenu }: { toggleFilterMenu: () => void}) {
+  const inputRef = useRef<HTMLInputElement>(null)
+  const [search, setSearch] = useState('')
   const router = useRouter()
   const searchParams = useSearchParams()
+  const pathname = usePathname()
+
+  const updateSearchParams = (value: string) => {
+    const rawSearchParams = new URLSearchParams(searchParams.toString())
+    rawSearchParams.set('search', value)
+    router.replace(`/?${rawSearchParams.toString()}`)
+  }
 
   const handleSearch = (e: any) => {
-    e.preventDefault();
-    const rawSearchParams = new URLSearchParams(searchParams.toString())
-    rawSearchParams.set('search', e.target.value)
-    router.replace(`/?${rawSearchParams.toString()}`)
+    e.preventDefault()
+    updateSearchParams(search)
+    inputRef.current?.blur()
+  }
+
+  const handleChange = (e: any) => {
+    console.log(pathname)
+    setSearch(e.target.value)
+    if (pathname === '/') {
+      updateSearchParams(e.target.value)
+    }
   }
 
   return (
@@ -23,11 +39,15 @@ export function SearchBar({ toggleFilterMenu }: { toggleFilterMenu: () => void})
       <div className={styles['search-icon']}>
         <FiSearch />
       </div>
-      <input
-        type="text"
-        onChange={handleSearch}
-        placeholder="Find a recipe"
-      />
+      <form onSubmit={handleSearch}>
+        <input
+          ref={inputRef}
+          onChange={handleChange}
+          placeholder="Find a recipe"
+          value={search}
+          inputMode="search"
+        />
+      </form>
       <Button
         className={styles['filter-icon']}
         variant="tertiary"
