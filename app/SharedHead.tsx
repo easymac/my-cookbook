cb948e0b944be2536b2699f09d29944a97cfe2e5
easymac <mac@ezm.ac>
2023-02-13 11:26:33 -0700

    Add head content (favicon, meta, etc)

---
+++ app/SharedHead.tsx
@@ -0,0 +1,25 @@
+import CONFIG from '@/cookbook.config'
+
+export function SharedHead() {
+  const URLs = {
+    16: `${CONFIG.faviconDirectory}/favicon-16x16.png`,
+    32: `${CONFIG.faviconDirectory}/favicon-32x32.png`,
+    appleTouchIcon: `${CONFIG.faviconDirectory}/apple-touch-icon.png`,
+    manifest: `${CONFIG.faviconDirectory}/site.webmanifest`,
+    favicon: `${CONFIG.faviconDirectory}/favicon.ico`,
+    msapplicationConfig: `${CONFIG.faviconDirectory}/browserconfig.xml`,
+  }
+
+  return (
+    <>
+      <link rel="icon" type="image/png" href={URLs[16]} sizes="16x16" />
+      <link rel="icon" type="image/png" href={URLs[32]} sizes="32x32" />
+      <link rel="apple-touch-icon" href={URLs.appleTouchIcon} sizes="180x180" />
+      <link rel="shortcut icon" href={URLs.favicon} />
+
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="mobile-web-app-capable" content="yes" />
+    </>
+  )
+}
--- cookbook.config.js
+++ cookbook.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   siteTitle: 'My Cookbook',
+  siteDescription: 'A collection of my recipes',
+  siteURL: 'https://mycookbook.com',
+  faviconDirectory: '/favicons/logo-a',
 }
