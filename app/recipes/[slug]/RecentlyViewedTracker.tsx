ec6a81f3b9856d1fccc815ba5336a520da7a48de
easymac <mac@ezm.ac>
2023-02-13 08:38:58 -0700

    Remove errant console statements

--- app/recipes/[slug]/RecentlyViewedTracker.tsx
+++ app/recipes/[slug]/RecentlyViewedTracker.tsx
@@ -4,9 +4,7 @@ import { useEffect } from 'react'
 export function RecentlyViewedTracker({ slug }: { slug: string }) {
   useEffect(() => {
     const recentlyViewed = JSON.parse(localStorage.getItem('recentlyViewed')) || []
-    console.log(recentlyViewed)
     const result = recentlyViewed.filter((recentSlug: string) => recentSlug !== slug)
-    console.log(result)
     result.unshift(slug)
       
     localStorage.setItem('recentlyViewed', JSON.stringify(result))
