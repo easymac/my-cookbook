3dfcd763faf458d88aceb4e398e7dab45cfd06bb
easymac <mac@ezm.ac>
2023-02-26 14:12:23 -0700

    Add twitter:card metatag for larger image in embeds

--- app/head.tsx
+++ app/head.tsx
@@ -13,6 +13,7 @@ export default async function Head() {
       <meta property="og:type" content="website" />
       <meta property="og:url" content={CONFIG.siteURL} />
       <meta property="og:image" content={`${CONFIG.siteURL}/favicons/og-image.png`} />
+      <meta name="twitter:card" content="summary_large_image" />
 
       <SharedHead />
     </>
