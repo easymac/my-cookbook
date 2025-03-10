41c6d1424c2cbacc6520d7520352c1d64bf0b85f
easymac <mac@ezm.ac>
2025-03-09 18:33:58 -0600

    Add new menu icon

--- components/global/MenuButton.tsx
+++ components/global/MenuButton.tsx
@@ -1,11 +1,11 @@
-import { TbMenu } from 'react-icons/tb'
 import { Button } from '@/components/ui/Button'
 import styles from '@/components/global/Menu.module.css'
+import MenuIcon from '@/icons/menu-icon'
 
 export function MenuButton({ onOpen }: { onOpen: () => void }) {
   return (
     <Button onClick={onOpen} className={styles['menu-button']}>
-      <TbMenu />
+      <MenuIcon />
     </Button>
   )
 }
---
+++ icons/menu-icon.tsx
@@ -0,0 +1,10 @@
+import { SVGProps } from 'react'
+
+export default function MenuIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg width="93" height="93" viewBox="0 0 93 93" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <path fillRule="evenodd" clipRule="evenodd" d="M11.625 34.875C11.625 32.7349 13.3599 31 15.5 31H77.5C79.6401 31 81.375 32.7349 81.375 34.875C81.375 37.0151 79.6401 38.75 77.5 38.75H15.5C13.3599 38.75 11.625 37.0151 11.625 34.875Z" fill="currentColor"/>
+      <path fillRule="evenodd" clipRule="evenodd" d="M34.875 58.125C34.875 55.9849 36.6099 54.25 38.75 54.25H77.5C79.6401 54.25 81.375 55.9849 81.375 58.125C81.375 60.2651 79.6401 62 77.5 62H38.75C36.6099 62 34.875 60.2651 34.875 58.125Z" fill="currentColor"/>
+    </svg>
+  )
+}
